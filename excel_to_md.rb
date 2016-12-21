225d7d37cbdaf39c1f996d29ddf7fad5d7ff2ced
uday <udkumar@hotmail.com>
2016-12-21 15:31:58 +0530

    initial script ready

---
+++ excel_to_md.rb
@@ -0,0 +1,39 @@
+#!/usr/bin/env ruby
+require 'roo'
+
+Dir.glob("**/*.xlsx") do |file|
+  xlsx = Roo::Spreadsheet.open(file)
+  bookname = xlsx.column(1)
+
+  xlsx.column(3).each do |cl|
+  	if (cl != "Verse")
+  		directory_name = bookname[1]
+  	  Dir.mkdir(directory_name) unless File.exists?(directory_name)
+      if cl[0]
+  	   output_name = "#{directory_name}/#{File.basename(cl[0], '.*')}.md"
+      end
+			output = File.open(output_name, 'w')
+
+      tn = xlsx.column(5)
+      tn_data = tn[1]
+      p_tn_data = tn_data.split(/\R+/)
+      split_bullet = p_tn_data.join('').split(/(?=•)/) 
+
+      iterate_split_bullet = split_bullet.each
+      iterate_split_bullet.next
+
+      loop do
+        begin
+          final_data = iterate_split_bullet.next
+          before_hyphan = final_data.partition('-').first.partition('•').last
+          after_hyphan  = final_data.partition('-').last
+          output << "#"+"#{before_hyphan}\n\n"
+          output << "#{after_hyphan} \n"
+        rescue StopIteration
+          break
+        end
+      end
+			output.close
+  	end
+  end
+end
