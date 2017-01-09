efb30250a1aba916d5f35374ad6e9f810680f520
uday <udkumar@hotmail.com>
2017-01-09 17:47:10 +0530

    markdown creating within require chapter

--- notes_to_markdown.rb
+++ notes_to_markdown.rb
@@ -3,87 +3,64 @@ require 'roo'
 
 Dir.glob("**/*.xlsx") do |file|
   xlsx = Roo::Spreadsheet.open(file)
+  chapter = ""
+  bookname = xlsx.column(1)
+  book_name = bookname[1]
+  verse = ""
+  book = {}
+  chapter = {}
+  verses = {}
 
-  bookname             = xlsx.column(1)
-  cahpter_number_array = xlsx.column(2).uniq
-  
-  cahpter_number_array.each do |chapter|
-    if (chapter != "Chapter")
-      book_name      = bookname[1] if bookname
-      chapter_number = chapter if (chapter != "Chapter")
+  check_chapter = ""
+  xlsx.each_with_pagename do |name, sheet|
+    sheet.each(book: "Book", chapter: "Chapter", verse: "Verse" , notes: "TRANSLATION" )  do |s|
+      if s[:chapter] != "Chapter"
+        verse = s[:verse].split("-")[0]
+        if(check_chapter != s[:chapter])
+          verses = {}
+          check_chapter = s[:chapter]
 
-      Dir.mkdir(book_name) unless File.exists?(book_name)
-      Dir.mkdir("#{book_name}/#{chapter_number}") unless File.exists?("#{book_name}/#{chapter_number}")
+          verses[verse] = s[:notes]
+          chapter[s[:chapter]] = verses
+          
 
-      xlsx.column(3).each do |md|
-        if (md != "Verse")
-          output_name = "#{book_name}/#{chapter_number}/#{File.basename(md.partition('-').first, '.*')}.md"
-          output      = File.open("#{output_name}", 'w')
-
-          xlsx.column(4).each_with_index do |item, index|
-            p_tn_data = item.split(/\R+/)
-            split_bullet = p_tn_data.join('').split(/(?=•)/) 
-
-            iterate_split_bullet = split_bullet.each
-            iterate_split_bullet.next
-
-            loop do
-              begin
-                final_data = iterate_split_bullet.next
-                before_hyphan = final_data.partition('-').first.partition('•').last # It will show only string which is before hyphan.
-                after_hyphan  = final_data.partition('-').last # It will show only string which is after hyphan.
-                # Need to add here logic so that obly require varse should go in file. 
-
-                output << "#"+"#{before_hyphan}\n\n"
-                output << "#{after_hyphan} \n"
-              rescue StopIteration
-                break
-              end
-            end
-          end
-          output.close
+        elsif(check_chapter == s[:chapter])
+          verses[verse] = s[:notes]
         end
       end
     end
   end
 
-  # xlsx.column(3).each_with_index do |cl, index|
-  #   if (cl != "Verse")
-  #     directory_name = bookname[1]
-  #     puts cl.partition('-').first
+  chapter.each do |chapter_v, v|
+    Dir.mkdir("#{book_name}") unless File.exists?("#{book_name}")
+    Dir.mkdir("#{book_name}/#{chapter_v}") unless File.exists?("#{book_name}/#{chapter_v}")
+      verse_m = chapter[chapter_v]
+      verse_m.each do |verse, v|
+        output_name = "#{book_name}/#{chapter_v}/#{File.basename(verse.partition('-').first, '.*')}.md"
+        output      = File.open("#{output_name}", 'w')
+        p_tn_data = verse_m[verse].split(/\R+/)
+        split_bullet = p_tn_data.join('').split(/(?=•)/) 
 
-  #     Dir.mkdir(directory_name) unless File.exists?(directory_name)
-  #     if cl.partition('-').first
-  #       output_name = "#{directory_name}/#{File.basename(cl.partition('-').first, '.*')}.md"
-  #     end
-  #     output = File.open(output_name, 'w')
+        iterate_split_bullet = split_bullet.each
+        iterate_split_bullet.next
 
-  #     xlsx.column(4).each_with_index do |item, index|
-  #       # tn_data = tn[index]
-  #       p_tn_data = item.split(/\R+/)
-  #       split_bullet = p_tn_data.join('').split(/(?=•)/) 
+        loop do
+          begin
+            final_data = iterate_split_bullet.next
+            before_hyphan = final_data.partition('-').first.partition('•').last # It will show only string which is before hyphan.
+            after_hyphan  = final_data.partition('-').last # It will show only string which is after hyphan.
+            # Need to add here logic so that obly require varse should go in file. 
 
-  #       iterate_split_bullet = split_bullet.each
-  #       iterate_split_bullet.next
-
-  #       loop do
-  #         begin
-  #           final_data = iterate_split_bullet.next
-  #           before_hyphan = final_data.partition('-').first.partition('•').last # It will show only string which is before hyphan.
-  #           after_hyphan  = final_data.partition('-').last # It will show only string which is after hyphan.
-  #           # Need to add here logic so that obly require varse should go in file. 
-
-  #           output << "#"+"#{before_hyphan}\n\n"
-  #           output << "#{after_hyphan} \n"
-  #         rescue StopIteration
-  #           break
-  #         end
-  #       end
-
-  #     end
-  #     output.close
-  #   end
-  # end
+            output << "#"+"#{before_hyphan}\n\n"
+            output << "#{after_hyphan} \n"
+          rescue StopIteration
+            break
+          end
+        end
+      output.close
+    end
+  end  
 end
+
 
 
