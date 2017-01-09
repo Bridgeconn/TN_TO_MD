be371ee5e3e07f6040cc4ecfd8a10f5338f9903c
uday <udkumar@hotmail.com>
2017-01-09 11:38:25 +0530

    merge folder and file code

--- notes_to_markdown.rb
+++ notes_to_markdown.rb
@@ -3,30 +3,48 @@ require 'roo'
 
 Dir.glob("**/*.xlsx") do |file|
   xlsx = Roo::Spreadsheet.open(file)
-  bookname = xlsx.column(1)
-  # puts bookname
 
+  bookname             = xlsx.column(1)
   cahpter_number_array = xlsx.column(2).uniq
-  # puts cahpter_number_array.size
-
+  
   cahpter_number_array.each do |chapter|
-    # puts chapter if (chapter != "Chapter")
+    if (chapter != "Chapter")
+      book_name      = bookname[1] if bookname
+      chapter_number = chapter if (chapter != "Chapter")
 
-    book_name      = bookname[1] if bookname
-    chapter_number = chapter if (cahpter_number_array && (chapter != "Chapter"))
+      Dir.mkdir(book_name) unless File.exists?(book_name)
+      Dir.mkdir("#{book_name}/#{chapter_number}") unless File.exists?("#{book_name}/#{chapter_number}")
 
-    Dir.mkdir(book_name) unless File.exists?(book_name)
-    Dir.mkdir("#{book_name}/#{chapter_number}") unless File.exists?("#{book_name}/#{chapter_number}")
+      xlsx.column(3).each do |md|
+        if (md != "Verse")
+          output_name = "#{book_name}/#{chapter_number}/#{File.basename(md.partition('-').first, '.*')}.md"
+          output      = File.open("#{output_name}", 'w')
 
-    xlsx.column(3).each do |md|
-      # puts md.partition('-').first if (md != "Verse")
-      dir = File.dirname("#{book_name}/#{chapter_number}")
-      output_name = "#{book_name}/#{chapter_number}/#{File.basename(md.partition('-').first, '.*')}.md" if (md != "Verse")
-      output = File.open("#{dir}/#{output_name}", 'w') if dir
-      
-      # output << "hello"
+          xlsx.column(4).each_with_index do |item, index|
+            p_tn_data = item.split(/\R+/)
+            split_bullet = p_tn_data.join('').split(/(?=•)/) 
+
+            iterate_split_bullet = split_bullet.each
+            iterate_split_bullet.next
+
+            loop do
+              begin
+                final_data = iterate_split_bullet.next
+                before_hyphan = final_data.partition('-').first.partition('•').last # It will show only string which is before hyphan.
+                after_hyphan  = final_data.partition('-').last # It will show only string which is after hyphan.
+                # Need to add here logic so that obly require varse should go in file. 
+
+                output << "#"+"#{before_hyphan}\n\n"
+                output << "#{after_hyphan} \n"
+              rescue StopIteration
+                break
+              end
+            end
+          end
+          output.close
+        end
+      end
     end
-
   end
 
   # xlsx.column(3).each_with_index do |cl, index|
@@ -40,9 +58,7 @@ Dir.glob("**/*.xlsx") do |file|
   #     end
   #     output = File.open(output_name, 'w')
 
-  #     xlsx.column(5).each_with_index do |item, index|
-  #       # puts item
-
+  #     xlsx.column(4).each_with_index do |item, index|
   #       # tn_data = tn[index]
   #       p_tn_data = item.split(/\R+/)
   #       split_bullet = p_tn_data.join('').split(/(?=•)/) 
@@ -69,22 +85,5 @@ Dir.glob("**/*.xlsx") do |file|
   #   end
   # end
 end
-
-
-
-# 153k
-
-# 13%
-
-# 3%
-
-# 13914
-
-
-# 5585
-
-
-# 12642
-
 
 
